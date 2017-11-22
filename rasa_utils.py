73a56f72fb4aeaf3999ded1d1bb767c8e68a6a09
keineahnung2345 <mimifasosofamire1123@gmail.com>
2017-11-22 08:56:19 +0800

    Update and rename rasa_utils to rasa_utils.py

--- rasa_utils.py
+++ rasa_utils.py
@@ -4,6 +4,8 @@ import requests
 import json
 
 def extract_entities(query_str):
+    # sample input: "天津市和平区西康路37号赛顿中心。"
+    # its output： ['天津市', '和平区', '西康路', '37号', '赛顿中心']
     response = requests.post("http://192.168.2.171:5002/parse", '{{"q":"{0}", "project": "default", "model": "model_20171121-135555"}}'.format(query_str).encode('utf-8'))
     parsed_j = json.loads(response.text)
     values = [e["value"] for e in parsed_j["entities"]]
